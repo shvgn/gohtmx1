f7909d7c0605aa5c8819b3a49204c0c8b15711b4
Eugene Shevchenko <dev@takogo.net>
2023-10-23 23:30:26 +0300

    go code with CDN
    Signed-off-by: Eugene Shevchenko <dev@takogo.net>

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/shvgn/gohtmx1
+
+go 1.21.3
+
+require (
+	github.com/a-h/templ v0.2.408
+	github.com/labstack/echo/v4 v4.11.2
+	k8s.io/apimachinery v0.28.3
+)
+
+require (
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"k8s.io/apimachinery/pkg/util/rand"
+)
+
+func newWordGen() *wordgen {
+	g := &wordgen{}
+	g.generate() // init
+	return g
+}
+
+type wordgen struct {
+	word string
+	mu   sync.RWMutex
+}
+
+func (s *wordgen) generate() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.word = strings.ToUpper(rand.String(12))
+}
+
+func (s *wordgen) get() string {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	return s.word
+}
+
+func main() {
+
+	state := newWordGen()
+	go func() {
+		for {
+			time.Sleep(3 * time.Second)
+			state.generate()
+		}
+	}()
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		component := page(state.get())
+		component.Render(context.Background(), c.Response())
+		return nil
+	})
+
+	e.GET("/fragments/code", func(c echo.Context) error {
+		component := Code(state.get())
+		component.Render(context.Background(), c.Response())
+		return nil
+	})
+
+	e.POST("/fragments/generate", func(c echo.Context) error {
+		state.generate()
+		component := Code(state.get())
+		component.Render(context.Background(), c.Response())
+		return nil
+	})
+	e.Logger.Fatal(e.Start(":3000"))
+}
